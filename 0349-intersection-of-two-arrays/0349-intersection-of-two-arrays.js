cdfa5543ef4c55e4d1e69743e5d5cc267ee05a80
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-09-05 15:06:25 -0400

    Time: 58 ms (52.64%), Space: 43.2 MB (53.64%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -5,17 +5,29 @@
  */
 var intersection = function(nums1, nums2) {
 
-    let dup = []
+    // let dup = []
 
-    let setNums1 = [...new Set(nums1)];
-    let setNums2 = [...new Set(nums2)];
+    // //make an array without duplicates
+    // let setNums1 = [...new Set(nums1)];
+    // let setNums2 = [...new Set(nums2)];
     
-    
-    for (let i = 0; i < setNums1.length; i++ ) {
-        if (setNums2.includes(setNums1[i])) {
-            dup.push(setNums1[i])
-        }
+    // //loop through array and see each element is in the other array
+    // for (let i = 0; i < setNums1.length; i++ ) {
+    //     if (setNums2.includes(setNums1[i])) {
+    //         dup.push(setNums1[i])
+    //     }
+    // }
+    // console.log(dup)
+    // return dup
+
+
+    let set = new Set(nums1)
+    let result = new Set()
+    for(let i=0; i<nums2.length; i++) {
+        if(set.has(nums2[i])) result.add(nums2[i])
     }
-    console.log(dup)
-    return dup
+    return Array.from(result)
+
+
+
 };
