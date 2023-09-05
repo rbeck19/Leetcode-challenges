14dd83340d54c99e36612e11330fbe6a5b83f125
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-09-05 15:12:43 -0400

    Time: 50 ms (87.03%), Space: 43.2 MB (53.64%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -5,29 +5,35 @@
  */
 var intersection = function(nums1, nums2) {
 
-    // let dup = []
+    let dup = []
 
-    // //make an array without duplicates
-    // let setNums1 = [...new Set(nums1)];
-    // let setNums2 = [...new Set(nums2)];
+    //make an array without duplicates
+    let setNums1 = [...new Set(nums1)];
+    let setNums2 = [...new Set(nums2)];
     
-    // //loop through array and see each element is in the other array
-    // for (let i = 0; i < setNums1.length; i++ ) {
-    //     if (setNums2.includes(setNums1[i])) {
-    //         dup.push(setNums1[i])
-    //     }
-    // }
-    // console.log(dup)
-    // return dup
+    //loop through array and see each element is in the other array
 
-
-    let set = new Set(nums1)
-    let result = new Set()
-    for(let i=0; i<nums2.length; i++) {
-        if(set.has(nums2[i])) result.add(nums2[i])
+    if (setNums1.length <= setNums2.length) {
+        for (let i = 0; i < setNums1.length; i++ ) {
+            if (setNums2.includes(setNums1[i])) {
+                dup.push(setNums1[i])
+            }
+        }
+    } else {
+        for (let i = 0; i < setNums2.length; i++ ) {
+            if (setNums1.includes(setNums2[i])) {
+                dup.push(setNums2[i])
+            }
+        }
     }
-    return Array.from(result)
 
 
 
+    return dup
+
+
+
+
+
+    
 };
