deb7dc647be52a4d251a5133491bfe95501a30d2
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-09-09 13:49:38 -0400

    Time: 175 ms (23.51%), Space: 51.6 MB (20.24%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,11 +2,16 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    
+var isPalindrome = function (x) {
+  //convert num into string
+  //split string into array of seperate elements
+  //reverse the order of the array
+  //join array for a reversed order string
+  let reverseString = x.toString().split("").reverse().join("");
 
-    let reverseString = x.toString().split("").reverse().join("")
+  //compare string version of given num to reversed string
+  return x.toString() === reverseString;
 
-    return x.toString() === reverseString
-
+  //above operation in one line
+  //return x.toString() === x.toString().split("").reverse().join("")
 };
