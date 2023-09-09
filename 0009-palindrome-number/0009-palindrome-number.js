feb5c2795c1e81bed4c3c16308bb200a396e4f35
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-09-09 13:55:53 -0400

    Time: 308 ms (5.01%), Space: 63.9 MB (5.07%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -7,11 +7,33 @@ var isPalindrome = function (x) {
   //split string into array of seperate elements
   //reverse the order of the array
   //join array for a reversed order string
-  let reverseString = x.toString().split("").reverse().join("");
+  //let reverseString = x.toString().split("").reverse().join("");
 
   //compare string version of given num to reversed string
-  return x.toString() === reverseString;
+  //return x.toString() === reverseString;
 
   //above operation in one line
   //return x.toString() === x.toString().split("").reverse().join("")
+
+
+    //do operation using a loop to compare the first to the last and move inward
+
+    let str = x.toString();
+
+    let i= 0;
+    let j= str.length -1;
+
+    while(i<j){
+        console.log(str[i] + "     " + str[j])
+        if(str[i] !== str[j]) {
+            return false
+        }
+        i++
+        j--
+    }
+
+    return true
+
+
+
 };
