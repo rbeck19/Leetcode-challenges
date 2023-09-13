ed1447daab1611de4cbf9262e012d66316592724
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-09-13 16:19:08 -0400

    Time: 105 ms (5.68%), Space: 50.2 MB (5.06%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+
+  let availableSpots = 0
+  for(let i = 0; i < flowerbed.length; i++) {
+    //skip if there is already a flower
+    if(flowerbed[i] == 1) {
+      continue
+    }
+    //check if the space before and after are available 
+    if( ( flowerbed[i - 1] == undefined || flowerbed[i - 1] == 0 ) && ( flowerbed[i + 1] == undefined || flowerbed[i + 1] == 0 ) ) {
+      console.log("i= " + i)
+      flowerbed[i] = 1
+      availableSpots++
+    }
+  }
+  return n <= availableSpots
+
+};
