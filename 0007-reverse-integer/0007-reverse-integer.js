87ac51c72c8c9e48958bd894733dd377bf2f1fc2
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-09-23 17:49:46 -0400

    Time: 60 ms (65.99%), Space: 43.9 MB (49.06%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -3,11 +3,14 @@
  * @return {number}
  */
 var reverse = function(x) {
+    //32 bit min and max int value 
     let min = -2147483648
     let max = 2147483648
-    if(x.length > 31) {
-        return 0
-    }
+    //IF x is less tham Zero 0
+    //make negative number positve 
+    //convert x to a string, split the string to an array reverse the array and join it
+    //add negative sign back onto string
+    //ELSE turn into string split it into array reverse the array and join 
     if(x < 0) {
         //make x positve
         x *= -1
@@ -16,5 +19,6 @@ var reverse = function(x) {
     } else {
         x = x.toString().split("").reverse().join("")
     }
+    //Ternary, if number is less than min or greater than max return 0 else return the reversed num
     return Number(x) < min || Number(x) > max ? 0 : x
 };
