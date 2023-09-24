925f80e6fb5bdbe6d0b32ca4f79e214604f06c98
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-09-24 11:46:36 -0400

    Time: 113 ms (51.27%), Space: 53.1 MB (68.22%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    
+    // create a an array of strs where each word is sorted aplhibeticaly
+    const sortedStrs = strs.map( word => word.split("").sort().join("") )       
+    const hashmap = {}
+
+    //loop through the sortedString array
+    //IF hashmap[sortedStrs]  does not exist set it with the value of the original word using the index
+    for(let i = 0; i < strs.length; i++) {
+        if(!hashmap[sortedStrs[i]]) {
+            hashmap[sortedStrs[i]] = [strs[i]]
+        } else {
+            hashmap[sortedStrs[i]].push(strs[i])
+        }
+    }
+
+    //return the values of all keys in the hashmap
+    return Object.values(hashmap)
+};
