1d5870c97befa2da1efa27746687ac812e0ad6e0
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-09-26 13:36:44 -0400

    Time: 57 ms (83.09%), Space: 45.8 MB (58.97%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+
+    //remove any and all that do not include  a-z A-Z 0-9 space and replace with nothing
+    //change characters to lower case
+    let noSpecialChars = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+    //reverse the string
+    let reversed = noSpecialChars.split("").reverse().join("")
+
+    return noSpecialChars == reversed
+};
