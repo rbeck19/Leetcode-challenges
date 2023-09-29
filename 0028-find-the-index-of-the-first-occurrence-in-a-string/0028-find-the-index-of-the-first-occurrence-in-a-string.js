5781a14118ea7198dd78f832fd6a113c277975a3
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-09-29 17:23:16 -0400

    Time: 40 ms (96.77%), Space: 42.1 MB (31.56%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    
+    let indexOf = haystack.indexOf(needle)
+
+    return indexOf
+
+};
