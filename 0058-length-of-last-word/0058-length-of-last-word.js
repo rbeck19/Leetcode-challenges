e64e1405a06f788bbf3b180e2baca246ea598990
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-09-30 13:33:51 -0400

    Time: 62 ms (5.03%), Space: 41.8 MB (67.48%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -4,6 +4,8 @@
  */
 var lengthOfLastWord = function(s) {
     
+    //split the array by spaces
+    //filter the array to only hold itemes greater than 0 length
     let arr = s.split(" ").filter((word)=>word.length > 0)
 
     return arr[arr.length-1].length
