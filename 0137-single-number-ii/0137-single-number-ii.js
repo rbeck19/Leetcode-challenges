4d35bf4c7ec005bbf8cd48dd57e7a85e4421186b
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-10-02 16:53:05 -0400

    Time: 68 ms (18.18%), Space: 43.9 MB (46.35%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.js
+++ 0137-single-number-ii/0137-single-number-ii.js
@@ -4,32 +4,20 @@
  */
 var singleNumber = function(nums) {
     
+    const hashmap = {}
 
-    // const hashmap = {}
-
-    // for(let i = 0; i < nums.length; i++) {
-    //     if(!hashmap[nums[i]]) {
-    //         hashmap[nums[i]] = 1
-    //     } else {
-    //         hashmap[nums[i]] += 1
-    //     }
-    // }
-
-    // for(let key in hashmap) {
-    //     if(hashmap[key] == 1) {
-    //         return key
-    //     }
-    // }
-
-
-    let result = nums;
-    for (let i = 0; i < nums.length; i++) {
-    result = nums;
-    result = result.filter((dataa) => dataa === nums[i]);
-    if (result.length === 1) {
-        return result[0];
-    }
+    for(let i = 0; i < nums.length; i++) {
+        if(!hashmap[nums[i]]) {
+            hashmap[nums[i]] = 1
+        } else {
+            hashmap[nums[i]] += 1
+        }
     }
 
+    for(let key in hashmap) {
+        if(hashmap[key] == 1) {
+            return key
+        }
+    }
 
 };
