9ef1b1ecc51b896172129caab94101bc8d0d4280
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-10-05 14:54:51 -0400

    Time: 70 ms (7.35%), Space: 48.2 MB (5.52%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.js
+++ 1217-relative-sort-array/1217-relative-sort-array.js
@@ -5,35 +5,17 @@
  */
 var relativeSortArray = function(arr1, arr2) {
     
+    let output = []
 
-    // sort the array arr1 in ascending order using sort()
-    let sortArr = arr1.sort((a, b) => a - b);
+    let temp = arr1.sort((a,b)=> a-b)
 
-    // initialize empty array result
-    let result = [];
-
-    // loop through every element of array arr2
-    for (let num of arr2) {
-
-        // find the first index of num in array sortArr using indexOf()
-        let firstIndex = sortArr.indexOf(num);
-
-        // find the last index of num in array sortArr using lastIndexOf()
-        let lastIndex = sortArr.lastIndexOf(num);
-
-        // remove the number according to firstIndex and lastIndex from sortArr using splice()
-        let copyArr = sortArr.splice(firstIndex, lastIndex - firstIndex + 1);
-
-        // push the all number of copyArr in result
-        result.push(...copyArr);
+    for(let i = 0; i < arr2.length; i++) {
+        temp = temp.filter(num => num !== arr2[i])
+        output.push(...arr1.filter(num => num == arr2[i]))
     }
 
-    // push the all remaining number of copyArr in result
-    result.push(...sortArr)
+    output.push(...temp)
 
-    // return the array result
-    return result
-
-
+    return output
 
 };
