7ad1f31e4a2aa8e2f72a8342a079b08dbefef7e7
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-10-05 14:58:02 -0400

    Time: 75 ms (5.52%), Space: 48.4 MB (5.52%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.js
+++ 1217-relative-sort-array/1217-relative-sort-array.js
@@ -6,19 +6,21 @@
 var relativeSortArray = function(arr1, arr2) {
     
     let output = []
-
-    
+    //place holder mimic arr1
     let temp = arr1
 
+    //loop through arr2 
+    //remove the items from temp array
+    //add the items to output
     for(let i = 0; i < arr2.length; i++) {
         temp = temp.filter(num => num !== arr2[i])
         output.push(...arr1.filter(num => num == arr2[i]))
-    }
+    }   
 
+    //sort the remaining numbers
     temp = temp.sort((a,b)=> a-b)
-
+    //add them to output aray
     output.push(...temp)
 
     return output
-
 };
