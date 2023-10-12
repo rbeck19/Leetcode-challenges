6f533c931cd15b960b65120ad0787a138463e98e
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-10-12 15:07:40 -0400

    Time: 45 ms (89.12%), Space: 41.6 MB (90.7%) - LeetHub

--- 0953-reverse-only-letters/0953-reverse-only-letters.js
+++ 0953-reverse-only-letters/0953-reverse-only-letters.js
@@ -3,13 +3,13 @@
  * @return {string}
  */
 var reverseOnlyLetters = function(s) {
-    
+  
+  //split the string at non characters, reverse the the order and re-join
   let string = s.split(/[^A-Za-z]+/g).join("").split("").reverse().join("")
-
   let newS = ""
-
   let patern = /[A-Za-z]/
-
+  //loop through s, if [i] is a Char add the new reversed Char to newS and add the counter
+  //else add the nonChar to the newS
   let counter = 0
   for(let i = 0; i < s.length; i ++) {
     if(patern.test(s[i])){
@@ -19,8 +19,5 @@ var reverseOnlyLetters = function(s) {
       newS += s[i]
     }
   }
-
-
   return newS
-
 };
