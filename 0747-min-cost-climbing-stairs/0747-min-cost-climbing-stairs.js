d83494972fc6f050a42607dc2603ba23bbff7a29
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-10-13 13:24:19 -0400

    Time: 54 ms (68.7%), Space: 41.7 MB (98.8%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.js
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.js
@@ -4,18 +4,15 @@
  */
 var minCostClimbingStairs = function(cost) {
     
-
-    let n = cost.length
-    let s1 = cost[0]
-    let s2 = cost[1]
+    let n = cost.length //length
+    let s1 = cost[0]    //step 1
+    let s2 = cost[1]    //step 2
 
     for(let i = 2; i < n; i++) {
         let current = cost[i] + Math.min(s1,s2)
-        console.log(current, s1, s2)
         s1 = s2
         s2 = current
     }
 
     return Math.min(s1,s2)
-
 };
