483126eb8c9316786495d3b47ee0ca78248e296c
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-10-14 15:01:30 -0400

    Time: 53 ms (47.97%), Space: 41.5 MB (93.11%) - LeetHub

---
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+    
+
+
+    let count = 0
+
+    for(let i = 0; i < nums.length; i++) {
+        //look at index i and check for pairs after it
+        for(let j = i+1; j < nums.length; j++) {
+            if(nums[i] == nums[j]) {
+                count++
+            }
+        }
+    }
+
+    return count
+
+
+};
