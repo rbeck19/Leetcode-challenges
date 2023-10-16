85a43c325f20ff3457ff638b4e274e1bff8b96b1
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-10-16 17:08:58 -0400

    Time: 3992 ms (20.19%), Space: 44.3 MB (17.7%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.js
+++ 0029-divide-two-integers/0029-divide-two-integers.js
@@ -5,10 +5,54 @@
  */
 var divide = function(dividend, divisor) {
 
-    let result = Math.trunc(dividend/divisor)
+    //test case
+    if(divisor === 0) return 0
+    if(divisor === 1) return Math.min(2147483647, dividend)
+    if(divisor === -1 && dividend > 1) return Math.max(-2147483648, -dividend)
+    if(divisor === -1 && dividend < 1) return Math.min(2147483647, Math.abs(dividend))
 
-    if(result > -2147483647 && result > 2147483647){
-        return 2147483647;
+
+    // let result = Math.trunc(dividend/divisor)
+    let dividentPos
+    let divisorPos
+
+    let result = 0
+
+    //track positive status of divident
+    if(dividend < 0) {
+        dividentPos = false
+        dividend = Math.abs(dividend)
+    } else {
+        dividentPos = true
     }
-    return result
+    //track positive status of divisor
+    if(divisor < 0) {
+        divisorPos = false
+        divisor = Math.abs(divisor)
+    } else {
+        divisorPos = true
+    }
+
+    while(dividend >= divisor) {
+        dividend -= divisor
+        result++
+    }
+
+
+    if( (dividentPos === true && divisorPos === true) || (dividentPos === false && divisorPos === false) ) {
+         //return result
+    }else {
+        //return -Math.abs(result)
+        result = -Math.abs(result)
+    }
+
+    if(result > 2147483647) {
+        return 2147483647
+    } else if (result < -2147483648) {
+        return -2147483648
+    } else {
+        return result
+    }
+
+   
 };
