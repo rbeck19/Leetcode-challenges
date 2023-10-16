1343ab4d31ea0de3c3cd01131fc5233db29743dc
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-10-16 15:05:22 -0400

    Time: 59 ms (17.81%), Space: 42.5 MB (16.01%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -3,20 +3,44 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
-    let res = [[1]];
-    for (let i = 1; i <= rowIndex; i++) {
-        let row = [];
-        let j = 0;
-        while (j < i + 1) {
-            if (j === 0 || j == i) {
-                row.push(1);
-            } else {
-                row.push(res[i-1][j-1] + res[i-1][j]);
-            }
-            j++;
-        }
-        res.push(row);
+    // let res = [[1]];
+    // for (let i = 1; i <= rowIndex; i++) {
+    //     let row = [];
+    //     let j = 0;
+    //     while (j < i + 1) {
+    //         if (j === 0 || j == i) {
+    //             row.push(1);
+    //         } else {
+    //             row.push(res[i-1][j-1] + res[i-1][j]);
+    //         }
+    //         j++;
+    //     }
+    //     res.push(row);
+    // }
+
+    // return res[rowIndex];
+
+
+    let tri = [[1]]
+
+    for(let i = 1; i <= rowIndex; i++) {
+        let row = [] // create the next row
+      for(let j = 0; j <= i; j++) {
+          //if creating the first or the last element in the row make it 1
+          if(j == 0 || j == i) {
+              row.push(1)
+          } else {
+            //   console.log("i:", i, "j:", j)
+            //   console.log(tri[i-1][j-1])
+              //push the element based on the previous row 1 before j and current j 
+              row.push(tri[i-1][j-1] + tri[i-1][j])
+          }
+          //add new row to triangle
+      }
+       tri.push(row)
     }
 
-    return res[rowIndex];
+    
+    return tri[rowIndex]
+
 };
