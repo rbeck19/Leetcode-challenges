a8a99497342e7e40ad582fdc17891a10f1f33397
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-10-17 14:37:20 -0400

    Time: 49 ms (69.92%), Space: 43 MB (9.77%) - LeetHub

--- 1274-number-of-days-between-two-dates/1274-number-of-days-between-two-dates.js
+++ 1274-number-of-days-between-two-dates/1274-number-of-days-between-two-dates.js
@@ -5,16 +5,18 @@
  */
 var daysBetweenDates = function(date1, date2) {
     
+    let timeDiff = (new Date(date1).getTime()) - (new Date(date2).getTime())
 
-    let d1 = new Date(date1).getTime()
-    let d2 = new Date(date2).getTime()
+    console.log(timeDiff)
+
+    //let d1 = new Date(date1).getTime()
+    //let d2 = new Date(date1).getTime()
 
 
-    let timeDiff = Math.abs(d1 - d2)
+    //let timeDiff = Math.abs(d1 - d2)
 
+    //let dayDiff = Math.abs(d1 - d2) / (1000 * 3600 * 24)
 
-    let dayDiff = Math.abs(d1 - d2) / (1000 * 3600 * 24)
-
-    return Math.abs(d1 - d2) / (1000 * 3600 * 24)
+    return Math.abs(timeDiff) / (1000 * 3600 * 24)
 
 };
