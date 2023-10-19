f55e46e30d8f1d02c9e887301251eaf0925941a9
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-10-19 14:04:23 -0400

    Time: 58 ms (34.09%), Space: 43.2 MB (33.28%) - LeetHub

---
+++ 0874-backspace-string-compare/0874-backspace-string-compare.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+    
+    let newS = []
+    let newT = []
+
+    for (let i = 0; i < s.length; i++){
+        if(s[i] === "#") {
+            newS.pop()
+        } else{
+            newS.push(s[i])
+        }
+    }
+
+    for (let i = 0; i < t.length; i++){
+        if(t[i] === "#") {
+            newT.pop()
+        } else{
+            newT.push(t[i])
+        }
+    }
+
+    return newS.join("") === newT.join("")
+};
