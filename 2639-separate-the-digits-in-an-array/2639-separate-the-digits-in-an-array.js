23721bccd9d4072e8730b65fd58d4e5fb209c839
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-10-21 13:09:17 -0400

    Time: 54 ms (96.71%), Space: 44.4 MB (80.66%) - LeetHub

--- 2639-separate-the-digits-in-an-array/2639-separate-the-digits-in-an-array.js
+++ 2639-separate-the-digits-in-an-array/2639-separate-the-digits-in-an-array.js
@@ -4,18 +4,18 @@
  */
 var separateDigits = function(nums) {
     
-
+    //join the entire array into one number
     let string = nums.join("")
-    console.log(string)
+    //spread the number and convert the elements from strings back into integers
     return [...string + ""].map(n=> + n)
 
-    const seperation = []
 
+    //set aside a output array
+    const seperation = []
+    //loop though each number and split it and add it to the array
     for(let i = 0; i < nums.length; i++) {
         let numSplit = [...nums[i] + ""].map(n=> + n)
         numSplit.forEach((element) => seperation.push(element))
     }
-
     return seperation
-
 };
