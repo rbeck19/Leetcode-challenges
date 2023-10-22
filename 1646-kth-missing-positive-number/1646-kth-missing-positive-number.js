5f803e9cebc2e464f2b2190a878cf9dbf7fa7122
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-10-22 13:57:23 -0400

    Time: 54 ms (59.33%), Space: 42.3 MB (55.99%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.js
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.js
@@ -5,21 +5,23 @@
  */
 var findKthPositive = function(arr, k) {
     
-    let missing = 0
-    let count = 0
-    let expected = 1
+    let missing = 0  //track the number missing
+    let count = 0   //track how many missing numbers
+    let expected = 1 //the number to look for
 
+    //loop through the array until you get to the k-th missing number in count
     for(let i = 0; count < k; i++) {
-
+        //if arr[i] is not the expected number
+        //add to count, missing number is the expected number
+        //set i to repeat on the same element
+        //if the missing k-th number is past the array length it will run by comparing expected to undefined until count hits the k-th element
         if(arr[i] != expected) {
             count += 1
             missing = expected
-            //expected += 1
             i = i -1
         }
-        expected += 1
+        expected += 1  //adjust the next expected number
     }
 
     return missing
-
 };
