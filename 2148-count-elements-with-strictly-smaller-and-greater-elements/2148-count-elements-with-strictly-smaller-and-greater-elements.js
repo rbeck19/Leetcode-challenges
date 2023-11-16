53f0a91b114a50b3cdca7d0c5889e1c33046bf3c
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-11-16 13:12:15 -0500

    Time: 140 ms (5.05%), Space: 50.6 MB (5.05%) - LeetHub

---
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countElements = function(nums) {
+    
+    //test case not enough nums 
+    if(nums.length < 3) {
+        return 0
+    }
+
+
+    nums.sort((a,b) => a -b)
+    console.log(nums)
+
+    let count = 0
+    //loop through all nums between the min and max number
+    for (let i = 1; i < nums.length - 1; i++) {
+        //if number is greater then the min and less than the max add to count
+        if (nums[i] > nums[0] && nums[i] < nums[nums.length - 1]) {
+            count += 1
+        }
+        console.log(nums[i], count)
+    }
+
+    return count
+
+};
