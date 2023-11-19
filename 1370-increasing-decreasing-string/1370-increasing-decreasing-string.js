544ca054d97d7315f14829845680ae84c784007a
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-11-19 15:27:19 -0500

    Time: 93 ms (14.49%), Space: 49.9 MB (11.23%) - LeetHub

--- 1370-increasing-decreasing-string/1370-increasing-decreasing-string.js
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.js
@@ -4,39 +4,42 @@
  */
 var sortString = function(s) {
     
+    //helper function to get letters forward
+    //loop through the object and push to output while there is value
+    //if no value delete the key
+    const loopForward = (hashmap) => {
+        for (const letter in hashmap) {
+        //console.log(`${letter}: ${hashmap[letter]}`);
+            if(hashmap[letter] > 0) {
+                output.push(letter)
+                hashmap[letter] -= 1
+            } else {
+                delete hashmap[letter]
+            }
 
-const loopForward = (hashmap) => {
-    for (const letter in hashmap) {
-    //console.log(`${letter}: ${hashmap[letter]}`);
-        if(hashmap[letter] > 0) {
-            output.push(letter)
-            hashmap[letter] -= 1
-        } else {
-            delete hashmap[letter]
-        }
-
-    }
-}
-
-const loopBackward = function(hashmap) {
-    let addLetter = []
-    for(const letter in hashmap) {
-        if(hashmap[letter] > 0) {
-            addLetter.push(letter)
-            hashmap[letter] -= 1
-        } else {
-            delete hashmap[letter]
         }
     }
-    addLetter = addLetter.reverse().join("")
-    output.push(addLetter)
-}
+    //helper function to get letters backward
+    //loop through foward and the reverse the order, then add that to the output
+    const loopBackward = function(hashmap) {
+        let addLetter = []
+        for(const letter in hashmap) {
+            if(hashmap[letter] > 0) {
+                addLetter.push(letter)
+                hashmap[letter] -= 1
+            } else {
+                delete hashmap[letter]
+            }
+        }
+        addLetter = addLetter.reverse().join("")
+        output.push(addLetter)
+    }
 
 
     const hashmap = {}
-
-    s = s.split("").sort().join("")
-
+    
+    s = s.split("").sort().join("") //order the string
+    //create a hashmap of letter occurances
     for (let i = 0; i < s.length; i++) {
         if(!hashmap[s[i]]) {
             hashmap[s[i]] = 1
@@ -47,7 +50,7 @@ const loopBackward = function(hashmap) {
 
 let output = []
 
-
+//whie the object has keys use the helper functions
 while(Object.keys(hashmap).length != 0) {
     loopForward(hashmap)
     loopBackward(hashmap)
