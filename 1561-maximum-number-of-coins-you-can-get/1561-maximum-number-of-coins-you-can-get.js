47de6e4ba9e1ad3a9ffd770be3f19a784f3d4808
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-11-24 13:17:48 -0500

    Time: 178 ms (46.51%), Space: 51.8 MB (91.86%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.js
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.js
@@ -4,28 +4,38 @@
  */
 var maxCoins = function(piles) {
     
+    // piles = piles.sort((a,b)=>a-b)
+    // //console.log(piles)
+    // //let alice = []
+    // let me = []
+    // //let bob = []
+
+    // while(piles.length > 0) {
+    //     //alice.push(piles[piles.length-1]) //move to alice
+    //     piles.pop()
+    //     if(piles.length > 0) {   //move to me
+    //         me.push(piles[piles.length-1])
+    //         piles.pop()
+    //     }
+    //     if(piles.length > 0) {   //move to bob
+    //         //bob.push(piles[0])
+    //         piles.shift()
+    //     }
+    // }
+    // answer = 0
+    // me.map((element)=> answer += element)
+    // return answer
+
+
     piles = piles.sort((a,b)=>a-b)
-    //console.log(piles)
-    //let alice = []
-    let me = []
-    //let bob = []
+    let startIndex = 0
+    let meIndex = piles.length - 2
+    let me = 0
     
-
-    while(piles.length > 0) {
-        //alice.push(piles[piles.length-1]) //move to alice
-        piles.pop()
-        if(piles.length > 0) {   //move to me
-            me.push(piles[piles.length-1])
-            piles.pop()
-        }
-        if(piles.length > 0) {   //move to bob
-            //bob.push(piles[0])
-            piles.shift()
-        }
+    while( startIndex < meIndex) {
+        me += piles[meIndex]
+        startIndex += 1 //move up one
+        meIndex -= 2 //move back two
     }
-
-    answer = 0
-    me.map((element)=> answer += element)
-    return answer
-
+    return me
 };
