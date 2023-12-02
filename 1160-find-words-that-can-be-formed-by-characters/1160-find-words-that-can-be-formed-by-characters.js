6c04d995e6ce2c17022bfdba8c683c62a89aae60
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-12-02 18:26:46 -0500

    Time: 197 ms (30.15%), Space: 50.6 MB (46.32%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.js
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.js
@@ -5,35 +5,6 @@
  */
 var countCharacters = function(words, chars) {
     
-
-
-    // let ch = {};
-    // let res = 0;
-    // for (const c of chars) { //hashmap od character count
-    //     ch[c] = 1 + (ch[c] || 0);
-    // }
-   
-
-    // for (const word of words) {
-    //     const copy = { ...ch };
-
-    //     for (const w of word) {
-    //         if (w in copy && copy[w] != 0) {
-    //             copy[w] -= 1;
-    //         }
-    //         else {
-    //             res -= word.length;
-
-    //             break;
-    //         }
-    //     }
-    //     res += word.length;
-    // };
-    // return res;
-
-
-
-
     let hashmap = {}
     let result = 0
 
@@ -45,25 +16,20 @@ var countCharacters = function(words, chars) {
         }
     }
 
-    // console.log(hashmap)
-
     for(let i = 0; i < words.length; i++) {
-        const temp = {...hashmap}
-        //console.log(hashmap)
-        //console.log("looking at:", words[i])
+        const temp = {...hashmap} //make a temp copy of hashmap
+
         for(let j = 0; j< words[i].length; j++) {
-            if(!temp[words[i][j]]) {
-                //console.log("BAD WORD", words[i][j], temp[words[i][j]])
+            if(!temp[words[i][j]]  || temp[words[i][j]] === 0) { //no letter in temp
                 break //break out of this word
             }
-            else if( temp[words[i][j]] > 0 && j === words[i].length -1) {
-                result += words[i].length
+            else if( temp[words[i][j]] > 0 && j === words[i].length -1) { //last letter in word and has a count
+                result += words[i].length  //add word length to result
             } else{
-                temp[words[i][j]] = temp[words[i][j]] - 1
+                temp[words[i][j]] = temp[words[i][j]] - 1 // take away one letter count from temp
             } 
         }
     }
-    //console.log( result )
     return result
 
 };
