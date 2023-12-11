67424211f4fda7f7fe633ce57435b6eae8eef491
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-12-11 13:26:04 -0500

    Time: 62 ms (40.96%), Space: 47.6 MB (19.68%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
@@ -5,26 +5,46 @@
 var findSpecialInteger = function(arr) {
     
     const hashmap = {}
-    let result = [0,0]
+    let result = [0,0] //use to track key value
 
-    //create a hashmap
+    //testcase for single value
+    if(arr.length === 1){
+        return arr[0]
+    }
+
+    //single loop to create hashmap and update result with lergest key value pair
     for(let i = 0; i < arr.length; i++) {
         if(!hashmap[arr[i]]) {
             hashmap[arr[i]] = 1
         } else {
             hashmap[arr[i]] += 1
-        }
-    }
-
-    //obtain the key with the largest value
-    for(const key in hashmap) {
-        console.log(key, hashmap[key])
-        if(hashmap[key] > result[1]) {
-            result[0] = key
-            result[1] = hashmap[key]
+            if(hashmap[arr[i]] > result[1]) {
+                result[0] = arr[i]
+                result[1] = hashmap[arr[i]]
+            }
         }
     }
 
     return result[0]
+
+    //create a hashmap
+    // for(let i = 0; i < arr.length; i++) {
+    //     if(!hashmap[arr[i]]) {
+    //         hashmap[arr[i]] = 1
+    //     } else {
+    //         hashmap[arr[i]] += 1
+    //     }
+    // }
+
+    // //obtain the key with the largest value
+    // for(const key in hashmap) {
+    //     console.log(key, hashmap[key])
+    //     if(hashmap[key] > result[1]) {
+    //         result[0] = key
+    //         result[1] = hashmap[key]
+    //     }
+    // }
+
+    // return result[0]
 
 };
