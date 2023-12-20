4ba920c0c766e428710dfa7d4b94b1e41ba90f98
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2023-12-20 10:53:42 -0500

    Time: 98 ms (16.67%), Space: 46.4 MB (60.74%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.js
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.js
@@ -5,10 +5,11 @@
  */
 var buyChoco = function(prices, money) {
     
-    prices = prices.sort((a,b)=> a-b)
-    //console.log(prices)
+    //sort in increasing order
+    prices = prices.sort((a,b)=> a-b)    
     
-    
+    //IF the two smllest prices is less than or equal to money return the left over money
+    //ELSE you cant but 2 with the money, return money
     if(prices[0]+prices[1] <= money) {
         return Math.abs( (prices[0]+prices[1]) - money )
     }   else {
