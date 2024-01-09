f3c266422d04e3b34b80e0597a46295f2c30828e
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2024-01-09 14:44:12 -0500

    Time: 128 ms (86.26%), Space: 51 MB (22.14%) - LeetHub

--- 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.js
+++ 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.js
@@ -4,34 +4,46 @@
  */
 var distinctDifferenceArray = function(nums) {
     
-    output = []
+    let result = []
 
     for(let i = 0; i < nums.length; i++) {
-        //looking at nums[i]
-  
-        let suffix = {}
-        let prefix = {}
-
-        //check all element at i and before 
-        for(let p = 0; p <= i; p++) {
-            //if no number in object add it otherwise ignore
-            if(!prefix[nums[p]]) {
-                prefix[nums[p]] = 1
-            }
-        }
-
-        //after check Suffix
-        for(let s = i + 1; s < nums.length; s++) {
-            //if no number in object add it otherwise ignore
-            if(!suffix[nums[s]]) {
-                suffix[nums[s]] = 1
-            }
-        }
-
-        output.push(Object.keys(prefix).length - Object.keys(suffix).length)
-        
+        //get size of array without duplicates from beging of array to current element
+        const prefixVal = new Set(nums.slice(0, i+1)).size
+        //get size of array without duplicated from element after i to the end
+        const sufixVal = new Set(nums.slice(i+1)).size
+        result.push(prefixVal - sufixVal)
     }
 
-    return output
+    return result
+
+
+    //nested loops to check before and after current element
+
+    // output = []
+
+    // for(let i = 0; i < nums.length; i++) {
+    //     //looking at nums[i]
+  
+    //     let suffix = {}
+    //     let prefix = {}
+
+    //     //check all element at i and before 
+    //     for(let p = 0; p <= i; p++) {
+    //         //if no number in object add it otherwise ignore
+    //         if(!prefix[nums[p]]) {
+    //             prefix[nums[p]] = 1
+    //         }
+    //     }
+
+    //     //after check Suffix
+    //     for(let s = i + 1; s < nums.length; s++) {
+    //         //if no number in object add it otherwise ignore
+    //         if(!suffix[nums[s]]) {
+    //             suffix[nums[s]] = 1
+    //         }
+    //     }
+    //     output.push(Object.keys(prefix).length - Object.keys(suffix).length)
+    // }
+    // return output
 
 };
