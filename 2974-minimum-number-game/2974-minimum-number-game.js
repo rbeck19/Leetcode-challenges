0e7a21247a1eb20b59e740cf935c6e638c1c2feb
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2024-01-28 12:11:27 -0500

    Time: 71 ms (54.29%), Space: 51.1 MB (17.82%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.js
+++ 2974-minimum-number-game/2974-minimum-number-game.js
@@ -12,8 +12,8 @@ var numberGame = function(nums) {
     
    //loop through nums and add them to arr based on the game rules
     for(let i =0; i < nums.length; i += 2){//increase by 2
-        arr.push(nums[i+1])
-        arr.push(nums[i])
+        arr.push(nums[i+1]) //first add the second number  Bob
+        arr.push(nums[i]) //than add the current Alice
     }
     return arr
 };
