6255565511619476ff2d4a728ac5b62cb5b84b22
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2024-02-09 12:48:04 -0500

    Time: 74 ms (73.42%), Space: 50.6 MB (41.77%) - LeetHub

---
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var largestDivisibleSubset = function(nums) {
+
+
+
+
+      let res = [];
+    let mem = new Array(nums.length).fill(-1);
+
+    function helper(index, curr, prev) {
+        if (curr.length > res.length) {
+            res = curr.slice();
+        }
+
+        for (let i = index; i < nums.length; i++) {
+            if (curr.length > mem[i] && nums[i] % prev === 0) {
+                mem[i] = curr.length;
+                curr.push(nums[i]);
+                helper(i + 1, curr, nums[i]);
+                curr.pop();
+            }
+        }
+    }
+
+    nums.sort((a, b) => a - b);
+    helper(0, [], 1);
+    return res;
+
+
+
+};
