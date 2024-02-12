1bdaa24eaa5d4e41e5308cf15df8d00e2d25ef61
rbeck19 <116828884+rbeck19@users.noreply.github.com>
2024-02-12 13:10:53 -0500

    Time: 59 ms (61.61%), Space: 50.9 MB (25.13%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -4,24 +4,44 @@
  */
 var majorityElement = function(nums) {
 
-        const hashmap = {}
 
-        for (let i = 0; i < nums.length; i++) {
-            if(!hashmap[nums[i]]) {
-                hashmap[nums[i]] = 1
-            } else {
-                hashmap[nums[i]] += 1
-            }
-        }
+    const hashmap = {}
 
-    let output = [0,0]
-
+    //fill the hashmap
+    for(let i = 0; i < nums.length; i++){
+        !hashmap[nums[i]] ? hashmap[nums[i]] = 1 : hashmap[nums[i]] += 1
+    }
+    
+    let res = [0,0]
+    //loop through the hashmap when the value is greater than what is in res, adjust res to new key value
     for(let key in hashmap) {
-        if(hashmap[key] > output[1]) {
-            output = [key, hashmap[key]]
+        if(hashmap[key] > res[1]) {
+            res = [key, hashmap[key]]
         }
     }
+    //return the first element of res which holds the majority element
+    return res[0]
 
-    return output[0]
+
+
+    //     const hashmap = {}
+
+    //     for (let i = 0; i < nums.length; i++) {
+    //         if(!hashmap[nums[i]]) {
+    //             hashmap[nums[i]] = 1
+    //         } else {
+    //             hashmap[nums[i]] += 1
+    //         }
+    //     }
+
+    // let output = [0,0]
+
+    // for(let key in hashmap) {
+    //     if(hashmap[key] > output[1]) {
+    //         output = [key, hashmap[key]]
+    //     }
+    // }
+
+    // return output[0]
 
 };
